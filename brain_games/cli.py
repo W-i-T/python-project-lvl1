d661a9d58645ee0d9215b51752731f806279b4bb
W-i-T <navichec@mail.ru>
2022-12-20 23:07:39 +0300

    fix: remove found errors cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string(f'Welcome to the Brain Games!\nMay I have your name?')
+    """Welcome to the game"""
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name?')
     print(f"Hello, {name}!")
-    
